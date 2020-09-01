0a172698786f1e99ee9c0af648d9bf69d6f2d721
Filipe Constantinov Menezes <fcmenezes87@gmail.com>
2020-09-01 18:46:34 +0100

    Fix double quote

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ DIR=$1
 TOKEN=${2:-$GITHUB_TOKEN}
 FORMAT='::{{ToLower .Severity.Name}} file={{.Position.FilePath}},line={{.Position.StartLine}},col={{.Position.StartColumn}}::{{.Message}}'
 
-codeownerslint -f $FORMAT -d $DIR -t $TOKEN
+codeownerslint -f "$FORMAT" -d "$DIR" -t "$TOKEN"
